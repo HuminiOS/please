d6d8f75dfdc3e3a2678364b02339efaa1a6d1f69
HuminiOS <971833711@qq.com>
2019-04-11 14:04:31 +0800

    update some text

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ const sleep = function(sec) {
 (async () => {
 
     for (;;) {
-        fs.appendFile('aa', 'addText', (err) => {
-            if (err) {
-                throw err;
-            }
-            console.log("File is updated.");
-        });
-    
-        fs.appendFileSync('./aa', 'addText')
+        try {
+            fs.appendFile('aa', 'addText', (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log("File is updated.");
+            });
         
-        shell.exec(`git add ./`);
-        // git commit --date="10 day ago" -m "Your commit message" 
-        shell.exec(`git commit --date="1000 day ago" -m "update some text"`);
-        sleep(1);
+            fs.appendFileSync('./aa', 'addText')
+            
+            shell.exec(`git add ./`);
+            // git commit --date="10 day ago" -m "Your commit message" 
+            shell.exec(`git commit --date="1000 day ago" -m "update some text"`);
+
+        } catch (error) {
+            console.error('error', error);
+        }
     }
 })();
