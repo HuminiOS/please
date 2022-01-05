87c497913074f4d9bdc171ac80dd16e9fc66aa44
HuminiOS <971833711@qq.com>
2022-01-05 12:42:31 +0800

    update some text

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const shell = require('shelljs');
+
+const sleep = function(sec) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res();
+        }, sec * 1000)
+    })
+};
+ (async () => {
+
+    for (;;) {
+        fs.appendFile('aa', 'addText', (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("File is updated.");
+        });
+    
+        
+        shell.exec(`git add ./`);
+        shell.exec(`git commit -m "update some text"`);
+        sleep(1);
+    }
+})();
